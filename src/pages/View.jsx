827e55fc14d42fde7a287f6468e32b68daeb2990
Abhijith Krishna A K <138287385+Abhijith-AK@users.noreply.github.com>
2025-01-08 13:53:07 +0530

    commit v5

--- src/pages/View.jsx
+++ src/pages/View.jsx
@@ -1,8 +1,12 @@
 import { useEffect, useState } from 'react'
 import Header from '../components/Header'
 import { useParams } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { addToWishList } from '../redux/slices/wishlistSlice'
 
 const View = () => {
+    const dispatch = useDispatch()
+    const userWishlist = useSelector(state => state.wishlistReducer)
     const { id } = useParams()
     console.log(id);
 
@@ -16,33 +20,44 @@ const View = () => {
         }
     }, [])
 
+    const handleWishlist = () => {
+        const existingProduct = userWishlist?.find(item => item?.id == id)
+        if (existingProduct) {
+            alert("Productt already in your Wishlist!!")
+        } else {
+            dispatch(addToWishList(product))
+        }
+    }
+
     return (
         <>
             <Header />
             <div className="flex flex-col mx-5">
                 <div className="grid grid-cols-2 items-center h-screen">
-                    <img src={product?.thumbnail} width={'450px'} height={'200px'} alt="" />
+                    <div className='flex flex-col items-center'>
+                        <img src={product?.thumbnail} width={'450px'} height={'200px'} alt="" />
+                        <div className="flex justify-evenly mt-5 w-full">
+                            <button onClick={handleWishlist} className="bg-blue-600 text-white p-2 rounded">Add to Wishlist</button>
+                            <button className="bg-green-600 text-white p-2 rounded">Add to Cart</button>
+                        </div>
+                    </div>
                     <div>
                         <h3 className='font-bold'>PID : {product?.id}</h3>
                         <h1 className='text-5xl font-bold'>{product?.title}</h1>
-                        <h4 className="font-bold text-red-600 text-2xl my-2">{product?.price}</h4>
+                        <h4 className="font-bold text-red-600 text-2xl my-2">$ {product?.price}</h4>
                         <h4>Brand : {product?.brand}</h4>
                         <h4>Category : {product?.category}</h4>
                         <p>
                             <span className="font-bold">Description</span> : {product?.description}
-                            <div className="flex justify-between mt-5">
-                                <button className="bg-blue-600 text-white p-2">Add to Wishlist</button>
-                                <button className="bg-green-600 text-white p-2">Add to Cart</button>
-                            </div>
                         </p>
                         <h3 className='font-bold mt-5 text-xl mb-2'>Client Reviews</h3>
                         {
                             product?.reviews?.map(item => (
                                 <div key={item?.date} className="shadow border rounded p-2 mb-2">
                                     <h5>
-                                        <span className='font-bold'>{item?.reviewerName}</span> : <span>{ item?.comment }</span>
+                                        <span className='font-bold'>{item?.reviewerName}</span> : <span>{item?.comment}</span>
                                     </h5>
-                                    <p>Rating: { item?.rating } <i className="fa-solid fa-star text-yellow-400"></i></p>
+                                    <p>Rating: {item?.rating} <i className="fa-solid fa-star text-yellow-400"></i></p>
                                 </div>
                             ))
                         }
--- src/pages/Wishlist.jsx
+++ src/pages/Wishlist.jsx
@@ -1,27 +1,43 @@
 import React from 'react'
 import Header from '../components/Header'
-import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { removeItem } from '../redux/slices/wishlistSlice'
 
 const Wishlist = () => {
+    const dispatch = useDispatch()
+    const userWishlist = useSelector(state => state.wishlistReducer)
+
     return (
         <>
             <Header />
             <div className="px-5" style={{ paddingTop: '100px' }} >
-                <>
-                    <h1 className='text-4xl text-red-600 font-bold'>My WishList</h1>
-                    <div className="grid grid-cols-4 gap-4 mt-5">
-                        <div className="rounded border p-2 shadow">
-                            <img src="https://media.istockphoto.com/id/1206806317/vector/shopping-cart-icon-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=1RRQJs5NDhcB67necQn1WCpJX2YMfWZ4rYi1DFKlkNA=" width={'100%'} height={'200px'} alt="" />
-                            <div className="text-center">
-                                <h3 className='text-xl font-bold'>Product name</h3>
-                                <div className="flex justify-evenly mt-3">
-                                    <button className="text-xl"><i className="fa-solid fa-heart-circle-xmark text-red-500"></i></button>
-                                    <button className="text-xl"><i className="fa-solid fa-cart-plus text-green-700"></i></button>
-                                </div>
+                {
+                    userWishlist?.length > 0 ?
+                        <>
+                            <h1 className='text-4xl text-red-600 font-bold'>My WishList</h1>
+                            <div className="grid grid-cols-4 gap-4 mt-5">
+                                {
+                                    userWishlist?.map(item => (
+                                        <div className="rounded border p-2 shadow">
+                                            <img src={item?.thumbnail} width={'100%'} height={'200px'} alt="" />
+                                            <div className="text-center">
+                                                <h3 className='text-xl font-bold'>{item?.name}</h3>
+                                                <div className="flex justify-evenly mt-3">
+                                                    <button onClick={() => dispatch(removeItem(item?.id))} className="text-2xl"><i className="fa-solid fa-heart-circle-xmark text-red-500"></i></button>
+                                                    <button className="text-2xl"><i className="fa-solid fa-cart-plus text-green-700"></i></button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    ))
+                                }
                             </div>
+                        </>
+                        :
+                        <div className="flex justify-center items-center h-screen">
+                            <img src="https://sa.adanione.com/~/media/Foundation/Adani/emptyImages/empty_cart.gif" alt="Empty Wishlist" />
+                            <h1 className="text-3xl text-red-600">Your Wishlist is empty!!!</h1>
                         </div>
-                    </div>
-                </>
+                }
             </div>
         </>
     )
